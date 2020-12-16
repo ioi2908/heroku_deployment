bac381c1b67e303275940986df13a7bca2c316c5
Stanley <2001stany@gmail.com>
2020-12-16 12:49:36 +0300

    setup.py file for pypi added

--- guidance.py
+++ guidance.py
@@ -1,13 +1,15 @@
 # by myself 
 
 # first stage
->git push -u origin master
->heroku login 
->heroku create HereDomainName
->hahahahah !!
+git push -u origin master
+heroku login 
+heroku create HereDomainName
+
+hahahahah !!
 connect the project with git and github (have the master access).that is after push the project 
 to the repo then push the repo to heroku ie > git push heroku master.
 make sure you have the required packages:
+
 -gunicorn > ( pip install gunicorn)
 -whitenoise> (pip install whitenoise)  (if you'll choose to)
 -psycopg2 > (pip install psycopg2-binary)
@@ -33,21 +35,29 @@ ALLOWED_HOSTS = ['127.0.0.1', 'yourdomain.herokuapp.com'] #you get it in >heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 # very important for db  (at the bottom):
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
 django_heroku.settings(locals())
 
 
 # third stage
 # some debuging hints:
   -remove pkg-resource in requirements.txt and you can uninstall it from the project
-  -                                     
+
+
+
+  # fourth stage
+  # update the db in heroku server
+  heroku run python manage.py makemigrations
+  heroku run python manage.py migrate
+
+
 debugging is an unavoidable process...hahah!
 nothing easy boi
 
-by 
+ 
 @maen
 @ioi
 
